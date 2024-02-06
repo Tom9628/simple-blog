7e532ac2ec5928f319fa6f3351f9a050aec51d8c
Xuzhi Wei <compro.xwei@gmail.com>
2024-02-06 10:18:46 -0600

    90%

--- routes/articles.js
+++ routes/articles.js
@@ -5,23 +5,46 @@ router.get('/new',(req,res)=>{
     res.render('articles/new',{article: new Article()})
 })
 
-router.get('/:id',(req,res)=>{
-    res.send(req.params.id)
+router.get('/edit/:id',(req,res)=>{
+    const article = await Article.findById(req.param.id)
+    res.render('articles/edit',{article: article})
+})
+
+router.get('/:slug',async (req,res)=>{
+    const article = await Article.findOne({ slug:req.params.slug})
+    if(article == null) res.redirect('/')
+    res.render('articles/show',{article:article})
 })
 router.post('/',async (req,res)=>{
-    let article = new Article({
-        title: req.body.title,
-        description:req.body.description,
-        markdown:req.body.markdown
-    })
-    try{
-        article = await article.save()
-        res.redirect(`/articles/${article.id}`)
-    }catch(e){
-        console.log(e)
-        res.render('articles/new',{article:article})
-    }
+   
     
 })
+
+router.put('/:id',(req,res)=>{
+
+})
+
+router.delete('/:id', async(req,res)=>{
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
+function saveArticleAndRedirect(path){
+    return (req,res)=>{
+        let article = new Article({
+            title:req.body.title,
+            description:req.body.description,
+            markdown:req.body.markdown
+        })
+    
+        try{
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        }catch(e){
+            // console.log(e)
+            res.render(`articles/${path}`,{article:article})
+        }
+    } 
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const methodOverride = require('method-override')
 const app = express()
+
 mongoose.connect('mongodb://localhost/blog',{
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    // useCreateIndex:true
 })
 
 app.set('view engine','ejs')
 
-app.use('/articles',articleRouter)
 app.use(express.urlencoded({extended:false}))
-app.get('/',(req,res) => {
+app.use(methodOverride('_method'))
+app.get('/',async (req,res) => {
     // res.send('Hello world')
-    const articles = [{
-        title:'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title:'Test Article2',
-        createdAt: new Date(),
-        description: 'Test description'
-    }]
+    const articles = await Article.find().sort({
+        createdAt:'desc' })
     res.render('articles/index',{articles:articles})
 })
-
+app.use('/articles',articleRouter)
 app.listen(5000)
