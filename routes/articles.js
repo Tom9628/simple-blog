c7b28269a5ab9724ac2f3f10bdd7b4556c9fe6a0
Sainzaya Batkhuu <sainzayabatkhuu@Sainzayas-MacBook-Pro.local>
2024-02-07 15:16:43 -0600

    insert file upload

--- routes/articles.js
+++ routes/articles.js
@@ -2,11 +2,27 @@ const express = require('express')
 const router = express.Router()
 const Article = require('./../models/article')
 const slugify = require('slugify')
+const multer = require('multer');
+
+// Multer setup
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'public/uploads/') // specify the directory where files will be uploaded
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname); // use the original filename
+    }
+  });
+
+
+const upload = multer({ storage: storage });
+
+
 router.get('/new',(req,res)=>{
     res.render('articles/new',{article: new Article()})
 })
 
-router.get('/edit/:id',async (req,res)=>{
+router.get('/edit/:id', upload.single('image'),async (req,res)=>{
     console.log(req.params.id)
     const article = await Article.findById(req.params.id)
     console.log(article)
@@ -18,7 +34,7 @@ router.get('/:slug',async (req,res)=>{
     if(article == null) res.redirect('/')
     res.render('articles/show',{article:article})
 })
-router.post('/',async (req,res,next)=>{
+router.post('/', upload.single('image'),async (req,res,next)=>{
     req.article = new Article()
     next()
 },saveArticleAndRedirect('new'))
@@ -37,6 +53,7 @@ function saveArticleAndRedirect(path){
     return async (req,res)=>{
         let article = req.article
         article.title = req.body.title,
+        article.image = req.body.image,
         article.description = req.body.description,
         article.markdown = req.body.markdown
         try{
